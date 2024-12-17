ab254159322b9d38ad1442e4be2b13c5f5ba3022
ttoannguyen <codehub.personal@gmail.com>
2024-12-17 20:52:50 +0700

    tile map + rule map

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    [SerializeField] private GameObject destroyVFX;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+}
