4ce2f1fde5e9b8822f4c349e98bbc8f9dd484681
ttoannguyen <codehub.personal@gmail.com>
2024-12-22 20:01:50 +0700

    Magic + Magic Staff

--- Assets/Scripts/Weapon/Staff.cs
+++ Assets/Scripts/Weapon/Staff.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class Staff : MonoBehaviour, IWeapon
 {
     [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject magicLaser;
+    [SerializeField] private Transform magicLaserSpawnPoint;
+
+    private Animator myAnimator;
+    readonly int AttackHash = Animator.StringToHash("Attack");
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         MouseFollowWithOffset();
     }
     public void Attack()
     {
-        Debug.Log("Staff Attack");
+        myAnimator.SetTrigger(AttackHash);
+    }
+
+    public void SpawnStaffProjectileAnimEvent()
+    {
+        GameObject newLazer = Instantiate(magicLaser, magicLaserSpawnPoint.position, Quaternion.identity);
+    }
+
+    public WeaponInfo GetWeaponInfo()
+    {
+        return weaponInfo;
     }
 
     private void MouseFollowWithOffset()
@@ -28,10 +49,5 @@ public class Staff : MonoBehaviour, IWeapon
         {
             ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 0, angle);
         }
-    }
-
-    public WeaponInfo GetWeaponInfo()
-    {
-        return weaponInfo;
     }
 }
