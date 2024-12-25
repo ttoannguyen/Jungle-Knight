44e8a8b7e633d6c7caa76a664ac4fb1407fbfe56
ttoannguyen <codehub.personal@gmail.com>
2024-12-25 13:39:29 +0700

    Screen Shake

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -38,6 +38,8 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int damageAmount, Transform hitTransform)
     {
         if (!canTakeDamage) { return; }
+
+        ScreenShakeManager.Instance.ShakeScreen();
         knockback.GetKnockedBack(hitTransform, knockBackThrustAmount);
         StartCoroutine(flash.FlashRoutine());
         canTakeDamage = false;
